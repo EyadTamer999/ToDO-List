abeea29732b6278da52cbb4a267f294bc3982229
EyadTamer999 <eyadhassan2002@gmail.com>
2023-02-04 13:58:39 +0200

    Added good looking css and buttons and comments in js

---
+++ app.js
@@ -0,0 +1,2 @@
+//take input from text field and date, assing it to an object enrty that will have: content, date, priority, isDone
+// when the add button is pressed it is added to the array of buttons sorted by highest priority

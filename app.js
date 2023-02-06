f323c9d825616e74b84b82b27831ae4ab46beae9
EyadTamer999 <eyadhassan2002@gmail.com>
2023-02-06 14:02:11 +0200

    started writing js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 //take input from text field and date, assing it to an object enrty that will have: content, date, priority, isDone
 // when the add button is pressed it is added to the array of buttons sorted by highest priority
+
+
+function addToDo() {
+    let txt = document.getElementById("input").value.toString();
+    let date = document.getElementById('date').value.toString();
+    let priority = document.getElementById('priority').value.toString();
+    //get our data then add it to a new obj
+    //console.log({txt,date,priority});
+
+}

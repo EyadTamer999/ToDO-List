d8131c7c5b6f30ca57e03fb0435d49547fa29989
EyadTamer999 <eyadhassan2002@gmail.com>
2023-02-14 00:28:53 +0200

    it worked...kinda...

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ function addToDo() {
                 obj = JSON.parse(data); //now it an object
                 obj.table.push({id: 2, square: 3}); //add some data
                 json = JSON.stringify(obj); //convert it back to json
-                fs.writeFile('myjsonfile.json', json, 'utf8', callback); // write it back
+                fs.writeFile('jsonfile.json', json, 'utf8', callback); // write it back
             }
         });
         console.log('added');

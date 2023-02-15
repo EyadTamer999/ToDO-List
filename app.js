1ea16e11120cc8ed6c1c93aa2481dee23206c100
EyadTamer999 <eyadhassan2002@gmail.com>
2023-02-15 16:11:09 +0200

    created setDate function to get current date and time

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // when the add button is pressed it is added to the array of buttons sorted by highest priority
 let itemID = 0;
 
-let itemsArray = []
+let itemsArray = new Array();
 
 function alarm() {
     // we will keep calling this function to keep track of date and time and compare that date and time
@@ -36,6 +36,8 @@ function addToDo() {
         'date': date,
         'priority': priority
     };
+
+    itemsArray += [itemID, txt, date, priority];
 
     let ol = document.getElementById('toDoListOrderedList');
     //checks if any text field is empty and sends an error message
@@ -118,5 +120,19 @@ function addToDo() {
         });
         console.log('added');
     }
+}
 
+
+function setDate(){
+    // For todays date;
+    Date.prototype.today = function () {
+        return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+    }
+
+// For the time now
+    Date.prototype.timeNow = function () {
+        return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+    }
+
+    document.getElementById('date').textContent = Date.prototype;
 }
